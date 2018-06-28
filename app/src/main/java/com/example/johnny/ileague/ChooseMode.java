eddc662f32c416553cb2922f5076f1167482f8fd
Daniel <daniel.mazzei@web.de>
2018-06-28 15:29:20 +0200

    Actionlisteners

--- app/src/main/java/com/example/johnny/ileague/ChooseMode.java
+++ app/src/main/java/com/example/johnny/ileague/ChooseMode.java
@@ -1,13 +1,39 @@
 package com.example.johnny.ileague;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
 
 public class ChooseMode extends AppCompatActivity {
 
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_mode);
+
+         button=(Button)findViewById(R.id.Button_Liga);
+         button.setOnClickListener(new android.view.View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),ModeLeague.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
+
 }
--- app/src/main/java/com/example/johnny/ileague/ModeLeague.java
+++ app/src/main/java/com/example/johnny/ileague/ModeLeague.java
@@ -1,13 +1,34 @@
 package com.example.johnny.ileague;
 
+
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class ModeLeague extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_league);
+
+        button=(android.widget.Button)findViewById(R.id.Button_Liga);
+        button.setOnClickListener(new android.view.View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),EnterResults.class);
+                startActivity(i);
+            }
+        });
     }
 }
